47f3b5d72587fc2bb101f47e27864cd2d037c782
Alexander Nookinov <anookinov@gmail.com>
2021-08-21 22:02:10 +0700

    add generics in struct and method definitions

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,96 @@ fn find_largest(list: &[i32]) -> i32 {
     largest
 }
 
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// fn largest_generic<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+struct PointT<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> PointT<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl PointT<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+#[derive(Debug)]
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+#[derive(Debug)]
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     // let mut largest = number_list[0];
@@ -36,4 +126,42 @@ fn main() {
 
     let result = find_largest(&number_list);
     println!("The largest number is {}", result);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    // let result = largest_generic(&number_list);
+    // println!("The largest number is {}", result);   
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+    // let result = largest_generic(&char_list);
+    // println!("The largest char is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    println!("{:#?}, {:#?}, {:#?}", both_integer, both_float, integer_and_float);
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let integer = Some(5);
+    let float = Some(5.0);
+    println!("{:#?}, {:#?}", integer, float);
+
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
+    println!("{:#?}, {:#?}", integer, float);
+
+    let p = PointT { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
 }
